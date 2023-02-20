b9e8e71d4790a88027c62b47b9849f83af6a17f6
Olga Ivanova <olgaiv.berlin@gmail.com>
2023-02-20 15:46:17 +0100

    frontend: added a not found page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import PandaDetail from './components/PandaDetail';
 import PandaList from './components/PandaList';
+import { NotFound } from './pages/NotFound';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <div>
-        <Routes>
-          <Route path='/' element={<PandaList />} />
-          <Route path='/pandas/:id' element={<PandaDetail />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path='/' element={<PandaList />} />
+        <Route path='/pandas/:id' element={<PandaDetail />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </BrowserRouter>
   );
 };
---
+++ frontend/src/pages/NotFound.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+interface NotFoundProps {}
+
+export const NotFound: React.FC<NotFoundProps> = ({}) => {
+  return (
+    <div>
+      <h1>404</h1>
+      <h2>Page not found</h2>
+    </div>
+  );
+};
